a10db24e92389007644e69de411a265fdb30fc92
yun629 <syoo9016naver@gmail.com>
2025-06-09 02:37:44 +0900

    Update types.d.ts

--- types.d.ts
+++ types.d.ts
@@ -1,11 +1,22 @@
 import { Connection } from "mongoose";
+
 declare module 'daisyui';
 
-declare global{
-    var mongoose:{
-        conn:Connection | null;
-        promise: Promise<Connection> | null;
+declare global {
+  var mongoose: {
+    conn: Connection | null;
+    promise: Promise<Connection> | null;
+  };
+  
+  namespace NodeJS {
+    interface ProcessEnv {
+      MONGODB_URL: string;
+      NEXT_AUTH_SECRET: string;
+      NEXT_PUBLIC_PUBLIC_KEY: string;
+      PRIVATE_KEY: string;
+      NEXT_PUBLIC_URL_ENDPOINT: string;
     }
+  }
 }
 
 export {}
