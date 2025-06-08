60fd8cc7b16cf73580467e9f1081e380ccdd6153
yun629 <syoo9016naver@gmail.com>
2025-06-09 02:34:56 +0900

    Update next-auth.d.ts

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,8 +1,27 @@
-declare module "next-auth"{
+import NextAuth, { DefaultSession } from "next-auth";
 
-    interface Session{
-        user:{
-            id:string;
-        }& DefaultSession["user"]
-    }
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      createdAt?: string;
+      updatedAt?: string;
+    } & DefaultSession["user"];
+  }
+
+  interface User {
+    id: string;
+    email: string;
+    name: string;
+    createdAt?: string;
+    updatedAt?: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    createdAt?: string;
+    updatedAt?: string;
+  }
 }
